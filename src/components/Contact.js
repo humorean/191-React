cdb27125b2feceea14fdad8517ccfe1c00aca319
Ming Huang <humorean@gmail.com>
2019-08-29 12:12:28 -0700

    added content to request band

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class Footer extends React.Component{
+class Contact extends React.Component{
     render(){
         return(
-            <div id='footer' className='footer aln-c'>
+            <div id='contact' className='contact aln-c'>
                 <p>191st Army Band | Tel: <a href="tel:925-875-4433">925-875-4433</a> | Email: <a href="mailto:jeremy.g.garrido.mil@mail.mil">jeremy.g.garrido.mil@mail.mil</a> |</p>
                 <p id="copyright">Copyright © 191st Army Band, All rights reserved.</p>
             </div>
@@ -11,4 +11,4 @@ class Footer extends React.Component{
     }
 }
 
-export default Footer;
+export default Contact;
--- src/components/RequestBand.js
+++ src/components/RequestBand.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import SectionText from './SectionText';
 
 class RequestBand extends React.Component {
     render(){
         return(
             <div>
-                Request Band
+                <SectionText data={{
+                    title: 'Request The Band',
+                    content: 'Fill out the band request form DD2536, save it on you local drive then attach it below to send band request.  Attach your completed DD2536 here:'
+                }}/>
             </div>
         )
     }
